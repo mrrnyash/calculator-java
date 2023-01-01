03d6faee2e27787136b5450b0d3db2a7739ccff9
Natalya Botyasova <mrrnyash@gmail.com>
2023-01-01 22:47:29 +0800

    Added correct number conversion

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,119 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-//        Scanner sc = new Scanner(System.in); //System.in is a standard input stream
-//        System.out.println("Enter an expression with spaces: ");
-//        String input = sc.nextLine();
-//        if (InputHandler.checkInputString(input)) {
-//            System.out.println(calc(input));
-//        } else {
-//            throw new Exception("Expression does not meet requirements");
-//        }
-        NumberConverter.showNumbers();
 
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine(); // Read string input from user
+        String result = calc(input);
+        System.out.println(result);
+
+        // Number conversion test
+//        Integer res = NumberConverter.romanToArabic("XLIV");
+//        if(res != null)
+//            System.out.println(res);
+//        else
+//            System.out.println("Error. Function return null!");
+//
+//        String res = NumberConverter.arabicToRoman("19");
+//        if(res != null)
+//            System.out.println(res);
+//        else
+//            System.out.println("Function return null");
+
+
+        // 800 tests
+//        String ops = "+-*/";
+//        System.out.println("arabic numbers");
+//        for (int i = 1; i <= 10; i++) {
+//            for (int j = 1; j <= 10; j++) {
+//                for (int k = 0; k < 4; k++) {
+//                    String testInput = String.valueOf(i) + " " + ops.charAt(k) + " " + String.valueOf(j);
+//                    System.out.println(testInput + " = " + calc(testInput));
+//                }
+//            }
+//        }
+//        System.out.println("roman numbers");
+//        for (int i = 1; i <= 10; i++) {
+//            for (int j = 1; j <= 10; j++) {
+//                for (int k = 0; k < 4; k++) {
+//                    String testInput = NumberConverter.arabicToRoman(String.valueOf(i)) + " " + ops.charAt(k) + " " + NumberConverter.arabicToRoman(String.valueOf(j));
+//                    System.out.println(testInput + " = " + calc(testInput));
+//                }
+//            }
+//        }
     }
 
+    // Calculations
     private static String calc(String input) throws Exception {
-        String[] expression = input.split("\\s+");
-        String result = "";
+        String firstNumber, secondNumber;
+        boolean bOper = false;
+        int posOper = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char cur = input.charAt(i);
+            if (cur == '+' || cur == '-' || cur == '*' || cur == '/') {
+                if (bOper) {
+                    throw new Exception("Expression must contain only one operator. Found more.");
+                } else {
+                    bOper = true;
+                    posOper = i;
+                }
+            }
+        }
 
-        return result;
+        // Split expression on operator as delimiter
+        int pos = posOper - 1;
+        while (pos > 0 && input.charAt(pos) == ' ')
+            pos--;
+        firstNumber = input.substring(0, pos + 1);
+        pos = posOper + 1;
+        while (pos < input.length() - 1 && input.charAt(pos) == ' ')
+            pos++;
+        secondNumber = input.substring(pos);
+
+        boolean romanOutput = false;
+        Integer aFirstNumber = 0, aSecondNumber = 0;
+
+        if (NumberConverter.isArabic(firstNumber) && NumberConverter.isArabic(secondNumber)) {
+            aFirstNumber = Integer.parseInt(firstNumber);
+            aSecondNumber = Integer.parseInt(secondNumber);
+        } else {
+            if (NumberConverter.isRoman(firstNumber) && NumberConverter.isRoman(secondNumber)) {
+                aFirstNumber = NumberConverter.romanToArabic(firstNumber);
+                aSecondNumber = NumberConverter.romanToArabic(secondNumber);
+                romanOutput = true;
+            } else {
+                throw new Exception("Both operands must be in the same number system!");
+            }
+        }
+
+        Integer result = 0;
+        char operator = input.charAt(posOper);
+        switch (operator) {
+            case '+':
+                result = aFirstNumber + aSecondNumber;
+                break;
+            case '-':
+                result = aFirstNumber - aSecondNumber;
+                break;
+            case '*':
+                result = aFirstNumber * aSecondNumber;
+                break;
+            case '/':
+                result = aFirstNumber / aSecondNumber;
+                break;
+        }
+
+        if (romanOutput) {
+            if (result <= 0) {
+                throw new Exception("Wrong result in the Roman number system!");
+                // return "Wrong Answer!"; // Used for test
+            } else
+                return NumberConverter.arabicToRoman(String.valueOf(result));
+        } else {
+            return String.valueOf(result);
+        }
     }
 
 }
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,45 +1,129 @@
-import java.util.Objects;
+import java.util.HashMap;
 
 
 class NumberConverter {
 
-    public static int romanToArabic(String romanNumber) {
-        Integer arabicNumber = 1;
-        for (Integer number : Reference.NUMBERS.keySet()) {
-            String value = Reference.NUMBERS.get(number);
-            if (Objects.equals(romanNumber, value)) {
-                arabicNumber = number;
-                break;
+    // Useful constants for number conversion
+    private static final String romanAlphabet = "IVXLCDM";
+    private static final Integer[] arabicNumbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] romanNumbers = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    private static final HashMap<Integer, String> arToRom = new HashMap<Integer, String>();
+
+    static {
+        arToRom.put(1000, "M");
+        arToRom.put(900, "CM");
+        arToRom.put(500, "D");
+        arToRom.put(400, "CD");
+        arToRom.put(100, "C");
+        arToRom.put(90, "XC");
+        arToRom.put(50, "L");
+        arToRom.put(40, "XL");
+        arToRom.put(10, "X");
+        arToRom.put(9, "IX");
+        arToRom.put(5, "V");
+        arToRom.put(4, "IV");
+        arToRom.put(1, "I");
+    }
+
+    private static final HashMap<String, Integer> romToAr = new HashMap<String, Integer>();
+
+    static {
+        romToAr.put("M", 1000);
+        romToAr.put("CM", 900);
+        romToAr.put("D", 500);
+        romToAr.put("CD", 400);
+        romToAr.put("C", 100);
+        romToAr.put("XC", 90);
+        romToAr.put("L", 50);
+        romToAr.put("XL", 40);
+        romToAr.put("X", 10);
+        romToAr.put("IX", 9);
+        romToAr.put("V", 5);
+        romToAr.put("IV", 4);
+        romToAr.put("I", 1);
+    }
+
+    // Comparison of two Roman characters
+    private static boolean romanCompare(char A, char B) {
+        return romanAlphabet.indexOf(A) < romanAlphabet.indexOf(B);
+    }
+
+
+    // Conversion Roman to Arabic
+    public static Integer romanToArabic(String rNumber) {
+        int result = 0;
+        // XXX -> 30
+        for (int i = 0; i < rNumber.length(); i++) {
+            char current = rNumber.charAt(i);
+            if (romanAlphabet.indexOf(current) == -1) // if current character is wrong
+                return null;
+            if (i < rNumber.length() - 1 && romanCompare(current, rNumber.charAt(i + 1)))
+                continue;
+            if (i > 0 && romanCompare(rNumber.charAt(i - 1), current))
+                result += romToAr.get(String.valueOf(current)) - romToAr.get(String.valueOf(rNumber.charAt(i - 1)));
+            else
+                result += romToAr.get(String.valueOf(current));
+        }
+        return result;
+    }
+
+    // Conversion Arabic to Roman
+    public static String arabicToRoman(String aNumber) {
+        int result = 0, p = 1;
+        String rNumber = "";
+        for (int i = aNumber.length() - 1; i >= 0; i--) {
+            char current = aNumber.charAt(i);
+            if (current >= '0' && current <= '9') {
+                result += (current - '0') * p;
+                p *= 10;
+            } else {
+                return null;
             }
         }
-        return arabicNumber;
-    }
-
-//    public static String arabicToRoman(int arabicNumber) {
-//        int p = 0;
-//        String romanNumber = "";
-//        while (arabicNumber != 0) {
-//            while (arabicNumber >= Reference.NUMBERS.get(p)) {
-//
-//            }
-//            p++;
-//        }
-//        return romanNumber;
-//    }
-
-    public static void showNumbers() {
-        for (Integer number : Reference.NUMBERS.keySet()) {
-            String key = number.toString();
-            String value = Reference.NUMBERS.get(number);
-            System.out.println(key + " " + value);
+        int pos = 0;
+        while (result != 0) {
+            while (result >= arabicNumbers[pos]) {
+                rNumber += romanNumbers[pos];
+                result -= arabicNumbers[pos];
+            }
+            pos++;
         }
+        return rNumber;
     }
 
-    public static String convertToRoman(int number) {
-        String romanNumber = "";
+    // Check if provided string is a correct Arabic number
+    public static boolean isArabic(String aNumber) {
+        for (int i = 0; i < aNumber.length(); i++)
+            if (aNumber.charAt(i) < '0' || aNumber.charAt(i) > '9')
+                return false;
+        return true;
+    }
 
+    // Check if provided string is a correct Roman number
+    public static boolean isRoman(String rNumber) throws Exception {
+        for (int i = 0; i < rNumber.length(); i++) {
+            if (romanAlphabet.indexOf(rNumber.charAt(i)) == -1) {
+                return false;
+            }
+            if (i > 0) {
+                char c1 = rNumber.charAt(i);
+                char c2 = rNumber.charAt(i - 1);
+                String current = "" + c2 + c1;
+                if (romanCompare(c2, c1)) {
+                    boolean ok = false;
+                    for (int j = 0; j < romanNumbers.length; j++) {
+                        if (romanNumbers[j].equals(current))
+                            ok = true;
+                    }
+                    if (!ok) {
+                        throw new Exception("Wrong format in Roman number " + rNumber);
+                    }
+                }
+            }
+        }
 
-        return romanNumber;
+        return true;
     }
 
 }
